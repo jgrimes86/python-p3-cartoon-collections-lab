b6a6a94a4d45f994e5c7d1688b722b90175f2540
Jim Grimes <jimgrimes86@gmail.com>
2023-10-23 12:57:54 -0400

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,28 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarf_names):
+    i = 1
+    while i <= len(dwarf_names):
+        for name in dwarf_names:
+            print(f"{i}. {name}")
+            i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    excited_calls = [f"{call.title()}!" for call in planeteer_calls]
+    return excited_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(planeteer_calls):
+    call_lengths = [len(call) for call in planeteer_calls]
+    longest_call = max(call_lengths)
+    if longest_call > 4:
+        return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list_of_strings):
+    cheese_list = []
+    for string in list_of_strings:
+        if string == ("cheddar" or "gouda" or "camembert"):
+            cheese_list.append(string)
+    if len(cheese_list) > 0:
+        return cheese_list[0]
+    else:
+        return None
